9b5b6b830fef63209f3b3d023877a9e247358475
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-02-10 15:03:51 -0800

    set up required endpoints

--- satellite-api.py
+++ satellite-api.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def base():
+# to fetch a satellite's latest TLE
+@app.get("/satellite_tle/{norad_id}")
+def get_satellite_tle():
     return "Hello"
 
+# to fetch all of a satellite's TLEs
+# meaning that we have to store all the old TLEs
+# per norad, maintain a dictionary of -> norad: [TLEs]
+@app.get("/satellite_tle/{norad_id}")
+def get_all_tles():
+    return None
+
+# to add a custom tle for a satellite
+# since we cannot access the public tle data due to restrictions
+# this custom tle will go in our local storage of all tles
+@app.post("/satellite_tle/{norad_id}")
+def add_custom_tle():
+    return None
