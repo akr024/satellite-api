99ef34b76933247dc11d0a789616805ea7ee12d7
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2026-02-10 23:43:45 -0800

    add epoch parser, converted to the requested format

--- satellite-api.py
+++ satellite-api.py
@@ -1,6 +1,31 @@
-from fastapi import FastAPI
+import datetime
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+import httpx, requests
 
 app = FastAPI()
+
+# ideally, in production, this would be redis cache
+# but this dictionary cache in-memory is technically faster, hence used.
+cache = {}
+
+def epoch_parser(raw_response):
+    year = int(raw_response["line1"][18:20])
+    year = 2000 + year if year < 57 else 1900 + year
+    day = float(raw_response["line1"][20:32])
+    epoch = datetime(year, 1, 1, tzinfo=datetime.timezone.utc) + datetime.timedelta(days=day - 1)
+    return str(epoch)
+
+@app.get("/")
+def fetch_from_celestrak():
+    response = requests.get("https://celestrak.org/NORAD/elements/gp.php?CATNR=54234&FORMAT=2LE")
+    resContent = response.text.strip().splitlines()
+    resObj = {
+        "line1": resContent[0],
+        "line2": resContent[1],
+        "epoch": "j"
+    }
+    return resObj
 
 # to fetch a satellite's latest TLE
 @app.get("/satellite_tle/{norad_id}")
